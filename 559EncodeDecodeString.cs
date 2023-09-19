c8e18776b3df026f14a64eb46ade699df8bc76da
JuananGarcia <juanangrodriguez@gmail.com>
2023-09-19 13:25:44 +0200

    Create 559EncodeDecodeString.cs

---
+++ 559EncodeDecodeString.cs
@@ -0,0 +1,25 @@
+public class Solution {
+    /*
+     * @param strs: a list of strings
+     * @return: encodes a list of strings to a single string.
+     */
+    public String encode(List<String> strs) {
+        // write your code here
+        string encoder;
+
+        foreach(var str in strs){
+            encoder = encoder + str.Length + "/" + str;
+        }
+
+        return encoder;
+
+    }
+
+    /*
+     * @param str: A string
+     * @return: decodes a single string to a list of strings
+     */
+    public List<String> decode(String str) {
+        // write your code here
+    }
+}
